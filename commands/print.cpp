61b9696a4a27b8a7f5ee9e8d4c877779a9a6a5f4
Stepan Zubkov <stepanzubkov@florgon.com>
2023-07-11 22:10:44 +0300

    feat(print): Show line numbers in print command by default.

--- commands/print.cpp
+++ commands/print.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <iomanip>
 #include "commands.h"
 #include "../buffer.h"
 #include "../parse.h"
@@ -8,8 +9,17 @@
 Print::Print(Buffer* buffer) : buffer(buffer) {}
 
 void Print::run(CommandArgs args) {
-    for (std::string& line: *(buffer->get_lines())) {
-        std::cout << line << '\n';
+    int line_numbers_width = 0;
+    std::size_t lines_count = buffer->get_lines()->size();
+    do {
+        line_numbers_width += 1;
+    } while (lines_count /= 10);
+    for (auto i = 0; i < buffer->get_lines()->size(); i++) {
+        std::cout << std::setw(line_numbers_width + 1) << i << ' ' << (*(buffer->get_lines()))[i] << '\n';
     }
 }
-void Print::help() {}
+void Print::help() {
+    std::cout << "p[rint] - Prints content of current buffer. Shows line numbers by default.\n";
+    std::cout << "Keywoard arguments:\n";
+    std::cout << "    n[umber]=t[rue] or f[alse] - Show line numbers or no.\n";
+}
