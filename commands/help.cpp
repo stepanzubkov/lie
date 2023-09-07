7f32911b5f1f20dc8ecc4988b69120cdc693e1ed
Stepan Zubkov <stepanzubkov@florgon.com>
2023-09-07 22:27:40 +0300

    feat(help): Show help by every unique commands in arguments.

--- commands/help.cpp
+++ commands/help.cpp
@@ -15,9 +15,11 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 #include <iostream>
 #include <map>
 #include <string>
+#include <algorithm>
 #include "commands.h"
 #include "../parse.h"
 
@@ -25,10 +27,15 @@ Help::Help(std::map<std::string, Command*>& commands, std::map<std::string, std:
     : commands(commands), commands_aliases(commands_aliases) {}
 
 void Help::run(CommandArgs args) {
+    std::vector<std::string> already_used_commands;
     for (auto arg: args.pos_args) {
         if (auto search = commands_aliases.find(arg); search != commands_aliases.end()) {
             arg = commands_aliases[arg];
         }
+        if (std::find(already_used_commands.begin(), already_used_commands.end(), arg) != already_used_commands.end())
+           continue;
+        already_used_commands.push_back(arg);
+
         if (auto search = commands.find(arg); search == commands.end()) {
             std::cerr << "Command \"" + arg + "\" not found.\n";
         } else {
