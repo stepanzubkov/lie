c5040c691ee93a474941acf05699ebc2adb835ae
Stepan Zubkov <stepanzubkov@florgon.com>
2023-09-07 18:52:35 +0300

    feat(licensing): Apply GPLv3-or-later license and add copyright notices to all files.

--- commands/commands.h
+++ commands/commands.h
@@ -1,5 +1,22 @@
-// Command base class and derived classes.
-// Each derived class represents one command.
+/*
+    Command base class and derived classes.
+    Each derived class represents one command.
+    Copyright (C) 2023 Stepan Zubkov <stepanzubkov@florgon.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <map>
 #include <string>
 #include "../buffer.h"
